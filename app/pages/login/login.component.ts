e17f023a02b2c123720ce02a120b4aca0fee22c4
Cristofer Flores <cristofer.flort@gmail.com>
2018-01-31 21:41:07 -0600

    Accessing to native mobile API's to handle some styling issues

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { User } from "../../shared/user/user";
 import { Page } from "ui/page";
 import { Color } from 'color';
 import { View } from 'ui/core/view';
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
 
 import { UserService } from "../../shared/user/user.service";
 
@@ -18,6 +20,8 @@ export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
   @ViewChild('container') container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
   constructor (
     private userService: UserService,
@@ -61,10 +65,24 @@ export class LoginComponent implements OnInit {
 
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
     let container = <View>this.container.nativeElement;
     container.animate({
       backgroundColor: this.isLoggingIn ? new Color('white') : new Color('#301217'),
       duration: 200
     });
+  }
+
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
   }
 }
